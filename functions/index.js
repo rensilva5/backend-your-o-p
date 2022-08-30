a87b8036a03a488db2376c5642405c876c5e13d9
rensilva5 <renzoasilva@gmail.com>
2022-08-30 15:47:34 -0400

    routes setup

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,21 @@
 const functions = require("firebase-functions");
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
+const { getTasks, createTask, updateTask, deleteTask } = require("./src/tasks");
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/test', (req, res) => {
-    res.send('🎆YeahhHi! first test on my final project👍🏼')
-})
+app.get('tasks', getTasks)
+app.post('/tasks', createTask)
+app.patch('task/:taskId', updateTask)
+app.delete('/task/:taskId', deleteTask)
+
+// app.get('/test', (req, res) => {
+//     res.send('🎆YeahhHi! first test on my final project👍🏼')
+// })
+
 
 exports.app = functions.https.onRequest(app) 
 
